6deac617ca049f45af5b51624439b796939bf53b
Luiz Paulo Cardoso <70473328+DevLuizP@users.noreply.github.com>
2022-08-08 22:49:01 -0300

    Adicionar arquivos de projeto.

---
+++ TrimmedString/Program.cs
@@ -0,0 +1,14 @@
+﻿string greeting = "     Hello, World!     ";
+Console.WriteLine($"{greeting}");
+
+//Metódo "Trim" remove todos os espaços vazios do início e fim da string
+string trimmedgreeting = greeting.Trim();
+Console.WriteLine($"{trimmedgreeting}");
+
+//Metódo "TrimStart" remove todos os espaços vazios do início da string
+trimmedgreeting = greeting.TrimStart(' ');
+Console.WriteLine($"{trimmedgreeting}");
+
+//Metódo "TrimEnd" remove todos os espaços vazios do fim da String
+trimmedgreeting = greeting.TrimEnd(' ');
+Console.WriteLine($"{trimmedgreeting}");
